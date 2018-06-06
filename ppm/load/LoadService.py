d771aee251e6cd7e8ad82a87505c001309d7fcc5
Fredrik Paulsson <paulsson.engelholm@gmail.com>
2018-06-06 22:55:03 +0200

    Added creation of dataframe from in-memory ppm excel-file

--- ppm/load/LoadService.py
+++ ppm/load/LoadService.py
@@ -1,6 +1,8 @@
 
 from datetime import datetime
 from . Factory import Factory
+from io import BytesIO
+import pandas as pd
 
 class LoadService:
     def __init__(self):
@@ -9,5 +11,16 @@ class LoadService:
     def execute(self):
         factory = Factory()
         download = factory.create_downloader()
-        excel_file = download.execute()
-        return excel_file
+        excel_file = BytesIO(download.execute())
+
+        xl = pd.ExcelFile(excel_file)
+
+        df = xl.parse("Fondstatistik",
+                      header=None,
+                      skiprows=21,
+                      names=['Id', 'Name',  '', 'One_month', 'Three_months', 'Six_months', 'Twelve_months',
+                     'Three_years', 'Five_years', 'Avg_five_years', 'Netto', 'Brutto', 'SD36',
+                     'Sharpe', 'Valuta', '', '', '', '',
+                     'Category', ''])
+
+        return df
