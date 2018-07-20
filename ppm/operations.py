adc44bc8b81f8cdae73a01e83a83023bc200fc71
Fredrik Paulsson <paulsson.engelholm@gmail.com>
2018-07-20 12:01:48 +0200

    Added filtering of fund names using regexp

--- ppm/operations.py
+++ ppm/operations.py
@@ -1,40 +1,50 @@
-
+from re import match
 from .load.LoadService import LoadService
 
+_orig_df = None
 _df = None
 
 _categories = None
 
 
 def load(month=None):
-    global _df
+    global _df, _orig_df
     ls = LoadService()
     _df = ls.execute(month)
     _df["Compound"] = (_df.Twelve_months +
                        _df.Six_months +
                        _df.Three_months +
                        _df.One_month) / 4
+    _orig_df = _df
 
 
 def categories():
     return _df.Category.unique()
 
 
-def filter(categories):
+def filter_categories(categories):
     global _categories
     _categories = categories
 
 
+def filter_name(regexp):
+    global _df
+    matches = _df.Fund.apply(lambda x: match(regexp, x) is not None)
+    _df = _df[matches]
+
+
 def reset():
-    global _categories
+    global _categories, _df
     _categories = None
+    _df = _orig_df
 
 
 def trend(nbr_funds=3):
     trend = lambda x: x.nlargest(nbr_funds, "Compound")  # noqa: E731
 
     group = _df.groupby("Category")
-    funds_per_category = group.apply(trend)[["Fund", "Compound"]]
+    funds_per_category = group.apply(trend)[["Fund", "Compound",
+                                             "SD36", "Sharpe"]]
 
     if _categories:
         filter = " or ".join(["Category=='{}'".format(c) for c in _categories])
@@ -45,10 +55,10 @@ def trend(nbr_funds=3):
     return funds_per_category
 
 
-def agg(nbr_picked_funds):
+def agg(nbr_funds):
     funds = trend(1)
     sorted_funds = funds.sort_values("Compound", ascending=False)
-    picked_funds = sorted_funds.head(nbr_picked_funds)
+    picked_funds = sorted_funds.head(nbr_funds)
     picked_funds.name = "Aggressive Global Growth {}".format(_df.name)
 
     return picked_funds
