050b353b8b7c02afd914791968851421ecd0ab96
Fredrik Paulsson <paulsson.engelholm@gmail.com>
2018-07-22 12:32:07 +0200

    Updated ppm downloading to handle version in excel file names

--- ppm/load/Download.py
+++ ppm/load/Download.py
@@ -3,19 +3,43 @@ import requests
 
 
 class Download:
+    SUCCESS_CODE = 200
+
     def __init__(self, url, date):
         self._url = url
         self._date = date
 
+    def _get_date_str(self, date):
+        return '{0:%Y-%m-%d}'.format(date)
+
+    def _create_link(self, pre_url, date, file_version=""):
+        url_frm = '{0}/{1}/Statistik%20Premiepension%20{2}{3}.xlsm'
+        complete_url = url_frm.format(pre_url, 
+                                      date.year, 
+                                      self._get_date_str(date),
+                                      file_version)
+        return complete_url
+    
+    def _create_io_error(self, url, response):
+        txt = "Failed to download {0}, since status code is {1}"
+        msg = txt.format(url, response.status_code)
+        return IOError(response.status_code, msg)
+
     def execute(self):
-        date_str = '{0:%Y-%m-%d}'.format(self._date)
-        url_frm = '{0}/{1}/Statistik%20Premiepension%20{2}.xlsm'
-        url = url_frm.format(self._url, self._date.year, date_str)
+        date_str = self._get_date_str(self._date)
+        print("Trying to dowload ", date_str)
 
-        req = requests.get(url)
-        if req.status_code is not 200:
-            txt = "Failed to download {0}, since status code is {1}"
-            msg = txt.format(url, req.status_code)
-            raise IOError(req.status_code, msg)
+        versions = ["", "v2", "v3", "v4"]
 
-        return req.content
+        for version in versions:            
+            url = self._create_link(self._url, self._date, "." + version)
+            resp = requests.get(url)
+            if resp.status_code is self.SUCCESS_CODE:
+                break
+
+        if resp.status_code is not self.SUCCESS_CODE:
+            raise self._create_io_error(url, resp)
+
+        print("Succeded downloading ", date_str)
+
+        return resp.content
