2dff036bf957607acfcba29035989e18306b2d61
Fredrik Paulsson <paulsson.engelholm@gmail.com>
2018-07-28 19:01:24 +0200

    Made packages of directories, the module Download now transforms the dataframe

--- aza/fund_summary/load/Download.py
+++ aza/fund_summary/load/Download.py
@@ -1,5 +1,7 @@
 
-from pandas import read_html
+from pandas import read_html, to_numeric
+
+# https://www.avanza.se/fonder/lista.html
 
 
 class Download:
@@ -12,8 +14,31 @@ class Download:
     def __init__(self, url, page):
         self._url = url
         self._page = page
-    
+
+    def _convert_col_to_numeric(self, df, column_name):
+        # The column may contain '-' and consists of strings
+        # remove all "-" in column
+        # Finally convert to numeric
+        df = df[df[column_name] != '-'].copy()
+        # Convert the column to numeric
+        df[column_name] = to_numeric(df[column_name])
+        return df
+
+    def _transform(self, df):
+        # Rename columns - aligned with columns in ppm
+        df.columns = ["", "Fund", "",
+                      "", "One_month", "Three_months",
+                      "Six_months", "Twelve_months", "Three_years",
+                      "Five_years", ""]
+        # drop all columns containing no description
+        df = df.drop("", 1)
+        df = self._convert_col_to_numeric(df, "Twelve_months")
+        df = self._convert_col_to_numeric(df, "Three_years")
+        df = self._convert_col_to_numeric(df, "Five_years")
+        return df
+
     def execute(self):
         request_url = self._url + "?" + self.ARGUMENTS.format(self._page)
         resp_df = read_html(request_url)[1]
-        return resp_df    
+        resp_df = self._transform(resp_df)
+        return resp_df
